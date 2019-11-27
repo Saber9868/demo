a1dccc4718d97b2d9e7616bdf931aeca226f2a07
Saber9868 <1129701961@qq.com>
2019-11-27 12:24:44 +0800

    Saberの理想乡

---
+++ Saberの理想乡/precache-manifest.24793f44e2a8aa421c834ab8976fc55e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981e32fbcd7a3edb3bd70716089b8240",
+    "url": "./index.html"
+  },
+  {
+    "revision": "428ca50e9b44201a9d0c",
+    "url": "./static/css/2.fb9ed26a.chunk.css"
+  },
+  {
+    "revision": "cb28c17045ad13a9febf",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "428ca50e9b44201a9d0c",
+    "url": "./static/js/2.e24cff7d.chunk.js"
+  },
+  {
+    "revision": "cb28c17045ad13a9febf",
+    "url": "./static/js/main.595d0373.chunk.js"
+  },
+  {
+    "revision": "84343dd762a069de827e",
+    "url": "./static/js/runtime-main.26b4583f.js"
+  }
+]);
